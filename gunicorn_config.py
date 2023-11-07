ca1e7dfb2dda99055578eecc145113cc090e950f
mukoma mpashi <93529921+mukoma-mpashi@users.noreply.github.com>
2023-11-07 08:12:51 +0200

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -4,4 +4,4 @@ from application import app
 workers = 4  # Number of worker processes 
 bind = '0.0.0.0:5000'  # Host and port for Gunicorn
 timeout = 120  # Maximum time a worker is allowed to process a request
-app = "application:app()"
+app = "application:app"
